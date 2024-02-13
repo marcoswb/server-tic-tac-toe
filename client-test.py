d45d47403b280c45a3eedcfc98c3301f7ac8b72e
marcos_wb <marcos_wb@outlook.com>
2024-02-13 11:42:28 -0300

    communication between client and server

--- client-test.py
+++ client-test.py
@@ -1,9 +1,19 @@
 import socket
+from time import sleep
 
 host = '127.0.0.1'
 port = 8000
-
+messages = [
+    'Iniciar',
+    'Teste',
+    'Teste 2',
+    'Python 3'
+]
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
+
+for message in messages:
+    client.send(str.encode(message))
+    sleep(5)
 
 print('passou')
--- src/controllers/handle_client.py
+++ src/controllers/handle_client.py
@@ -1,9 +1,13 @@
+import json
+
+
 class Client:
 
     def __init__(self, ip_client, client):
         """
         Class to handle client connection
         """
+        self.SIZE_BUFFER_PACKETS = 1024
         self.__ip_client = ip_client
         self.__client = client
 
@@ -11,3 +15,21 @@ class Client:
 
     def handle_client(self):
         print(f'Conectado a {self.__ip_client}')
+
+        data = self.wait_for_data_client()
+        while data:
+            print(data)
+            data = self.wait_for_data_client()
+
+        self.close_client()
+
+    def wait_for_data_client(self):
+        """
+        Return the data that the client sent
+        """
+        response = self.__client.recv(self.SIZE_BUFFER_PACKETS).decode('utf8')
+        return response
+
+    def close_client(self):
+        print(f'Conexão com o client {self.__ip_client} finalizada')
+        self.__client.close()
